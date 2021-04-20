a4a0dd4230564903b87bbdfd7476128dfd660b37
Luca <luzenobi@gmail.com>
2021-04-20 19:08:09 +0200

    Added form validation to profile

--- src/main/resources/static/components/profile/niiu-profile.js
+++ src/main/resources/static/components/profile/niiu-profile.js
@@ -2,41 +2,47 @@
 
   const template = `
     <div class="main-area-container">
-      <div>
-        <h2>User data</h2>
-        <form v-on:submit.prevent="onUpdateProfile">
-          <md-field>
-              <label>Username</label>
-              <md-input v-model="user.username" class="initial"></md-input>
-          </md-field>
-          <md-field>
-              <label>Email</label>
-              <md-input v-model="user.email"></md-input>
-          </md-field>
-          <md-button class="md-raised md-primary" type="submit">Update</md-button>
-        </form>
-      </div>
-      
-      <div>
-        <h2>Password</h2>
-        <form v-on:submit.prevent="onUpdatePassword">
-          <!--<md-field>
-              <label>Old password</label>
-              <md-input v-model="passwords.oldPassword" 
-                        type="password"></md-input>
-          </md-field>-->
-          <md-field>
-              <label>New password</label>
-              <md-input v-model="passwords.newPassword"
-                        type="password"></md-input>
-          </md-field>
-          <md-field>
-              <label>Repeat new password</label>
-              <md-input v-model="passwords.repeatNewPassword"
-                        type="password"></md-input>
-          </md-field>
-          <md-button class="md-raised md-accent" type="submit">Update</md-button>
-        </form>
+      <div class="padded-view">
+        <div>
+          <h2>User data</h2>
+          <form v-on:submit.prevent="onUpdateProfile">
+            <md-field :class="usernameClasses">
+                <label>Username</label>
+                <md-input v-model="$v.user.username.$model"
+                          class="initial"></md-input>
+                <span class="md-error">{{usernameErrorText}}</span>
+            </md-field>
+            
+            <md-field :class="emailClasses">
+                <label>Email</label>
+                <md-input v-model="$v.user.email.$model"></md-input>
+                <span class="md-error">{{emailErrorText}}</span>
+            </md-field>
+            <md-button class="md-raised md-primary" type="submit">Update</md-button>
+          </form>
+        </div>
+        
+        <div>
+          <h2>Password</h2>
+          <form v-on:submit.prevent="onUpdatePassword">
+            <!--<md-field>
+                <label>Old password</label>
+                <md-input v-model="passwords.oldPassword" 
+                          type="password"></md-input>
+            </md-field>-->
+            <md-field>
+                <label>New password</label>
+                <md-input v-model="passwords.newPassword"
+                          type="password"></md-input>
+            </md-field>
+            <md-field>
+                <label>Repeat new password</label>
+                <md-input v-model="passwords.repeatNewPassword"
+                          type="password"></md-input>
+            </md-field>
+            <md-button class="md-raised md-accent" type="submit">Update</md-button>
+          </form>
+        </div>
       </div>
     </div>
   `;
@@ -44,13 +50,43 @@
   Vue.component('niiu-profile', {
     props: [],
     template: template,
+    computed: {
+      usernameClasses: function () {
+        return !this.$v.user.username.required || !this.$v.user.username.minLength ? 'md-invalid' : '';
+      },
+      usernameErrorText: function () {
+        if (!this.$v.user.username.required) return 'Field is required';
+        else if (!this.$v.user.username.minLength) return `Name must have at least ${this.$v.user.username.$params.minLength.min} letters.`;
+        else return '';
+      },
+      emailClasses: function () {
+        return !this.$v.user.email.required || !this.$v.user.email.valid ? 'md-invalid' : '';
+      },
+      emailErrorText: function () {
+        if (!this.$v.user.email.required) return 'Field is required';
+        else if (!this.$v.user.email.valid) return 'Must be a valid email';
+        else return '';
+      }
+    },
     data: function () {
       return {
-        user: {},
+        user: { },
         passwords: {
           /*oldPassword: '',*/
           newPassword: '',
           repeatNewPassword: ''
+        }
+      }
+    },
+    validations: {
+      user: {
+        username: {
+          required: validators.required,
+          minLength: validators.minLength(4)
+        },
+        email: {
+          required: validators.required,
+          valid: validators.email
         }
       }
     },
@@ -60,6 +96,8 @@
     },
     methods: {
       onUpdateProfile: async function () {
+        if (this.$v.$invalid) return;
+
         try {
           await UserApi.updateUser({
             username: this.user.username,
