a641e75448b2bc2198bcd91211d49222a95f4e9b
Davide Zenobi <davidezenobigarcia@gmail.com>
2021-05-06 19:30:11 +0200

    Front chats output

--- src/main/resources/static/components/chats/niiu-chats.js
+++ src/main/resources/static/components/chats/niiu-chats.js
@@ -1,9 +1,34 @@
 (() => {
 
   const template = `
-    <md-field>
+    <md-list class="md-double-line">
+    
+      <md-subheader>Chats</md-subheader>
+    
+      <md-list-item>
+        <md-icon>add</md-icon>
+        <span class="md-list-item-text">Create chat</span>
+      </md-list-item>
+      
+        <md-list-item v-for="chat in chats">
+          <md-avatar class="md-avatar-icon md-primary">
+            <md-icon>home</md-icon>
+          </md-avatar>
+          
+          <div class="md-list-item-text">
+            <span>{{chat.username}}</span>
+            <p>I'll be in your neighborhood doing errands this week. Do you want to meet?</p>
+          </div>
+          
+          <md-button class="md-icon-button md-list-action">
+            <md-icon class="md-primary">star_border</md-icon>
+          </md-button>
+          <md-button class="md-icon-button md-list-action">
+            <span class="material-icons ni-icon-danger">delete</span>
+          </md-button>
+        </md-list-item>
         
-    </md-field>
+    </md-list>
   `;
 
   Vue.component('niiu-chats', {
@@ -11,15 +36,12 @@
     template: template,
     data: function (){
       return {
-        chat: {
-          username: '',
-          groupId: '',
-          avatar: ''
-        }
+        chats: { }
       };
     },
     mounted: async function () {
-
+      const response = await ChatApi.getChatsByUserId();
+      this.chats = response.data;
     }
   });
 
