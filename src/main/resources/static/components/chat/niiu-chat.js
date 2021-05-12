3a1feb341435137df1c0ebd1771eb90235e8c8e0
Davide Zenobi <davidezenobigarcia@gmail.com>
2021-05-12 09:04:48 +0200

    Primeras cosas chat

--- src/main/resources/static/components/chat/niiu-chat.js
+++ src/main/resources/static/components/chat/niiu-chat.js
@@ -1,23 +1,21 @@
 (() => {
 
   const template = `
-    <div class="main-area-container">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
-    </div>
+    <md-list>
+      <md-list-item>
+        <p>HOLA</p>
+      </md-list-item>
+    </md-list>
   `;
 
   Vue.component('niiu-chat', {
     props: [],
     template: template,
     data: function () {
-      return { }
+      return {
+        messages: { },
+        user: { }
+      }
     },
     mounted: async function () {
 
--- src/main/resources/static/components/chats/niiu-create-chats.js
+++ src/main/resources/static/components/chats/niiu-create-chats.js
@@ -1,14 +1,18 @@
 (() => {
 
   const template = `
-    <md-list>
-      <md-list-item v-for="user in users">
-        <md-avatar>
+    <md-list class="create-chats-container">
+      <md-list-item v-for="user in users" class="create-chats-chat">
+        <md-avatar class="md-large">
           <img :src="getUserAvatar(user.id)">
         </md-avatar>
         <div class="md-list-item-text">
-          <span>{{user.username}}</span>
+          <span class="create-item">{{user.username}}</span>
         </div>
+        <md-button class="create-chats-button" 
+                   @click="$emit('chat-clicked')">
+          <span>TALK</span>
+        </md-button>
       </md-list-item>
     </md-list>
   `;
@@ -24,7 +28,6 @@
     },
     mounted: async function () {
       const response = await UserApi.getAll();
-      console.log(response.data);
       this.users = response.data;
     },
     methods: {
